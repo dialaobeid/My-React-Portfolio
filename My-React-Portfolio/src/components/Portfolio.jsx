27802258f351664584a19a524da2e449c09c6547
dobeid <dialajo9@gmail.com>
2024-05-23 18:26:54 -0700

    changes in css, added 4 more projects under portfolio section and fixed resume section

--- My-React-Portfolio/src/components/Portfolio.jsx
+++ My-React-Portfolio/src/components/Portfolio.jsx
@@ -1,23 +1,55 @@
 import React from 'react';
 import Project from './Project';
 import project1Image from '../assets/images//Project1.jpg';
-import project2Image from '../assets/images//Project.jpg';
+import project2Image from '../assets/images//Project2.jpg';
+import project3Image from '../assets/images//Project3.jpg';
+import project4Image from '../assets/images//Project4.jpg';
+import project5Image from '../assets/images//Project5.jpg';
+import project6Image from '../assets/images//Project6.jpg';
 
 function Portfolio() {
   const projects = [
     {
-      // title: 'Project 1',
+       title: 'Project 1',
       image: project1Image,
       deployedLink: 'https://young-bayou-05349-1a4b47be4457.herokuapp.com/',
       githubLink: 'https://github.com/dialaobeid/FitTrack',
     },
 
     {
-     // title: 'Project 2',
+      title: 'Project 2',
       image: project2Image,
       deployedLink: 'https://nimo20.github.io/Geek-Humor/',
       githubLink: 'https://github.com/Nimo20/Geek-Humor',
-    }
+    },
+
+    {
+       title: 'Project 3',
+       image: project3Image,
+       deployedLink: 'https://dialas-pwa-text-editor.onrender.com/',
+       githubLink: 'https://github.com/dialaobeid/Dialas-PWA-Text-Editor',
+     },
+
+     {
+       title: 'Project 4',
+       image: project4Image,
+       deployedLink: 'https://secure-harbor-04337-db2a2516c78a.herokuapp.com/notes',
+       githubLink: 'https://github.com/dialaobeid/My-Note-Taker/tree/main',
+     },
+
+     {
+       title: 'Project 5',
+       image: project5Image,
+       deployedLink: 'https://github.com/dialaobeid/Portfolio-Challenge/tree/main',
+       githubLink: 'https://dialaobeid.github.io/Portfolio-Challenge/',
+     },
+
+     {
+       title: 'Project 6',
+       image: project6Image,
+       deployedLink: 'https://dialaobeid.github.io/Dialas-Weather-Dashboard/',
+       githubLink: 'https://github.com/dialaobeid/Dialas-Weather-Dashboard',
+     },
   ];
 
   return (
--- My-React-Portfolio/src/components/Resume.jsx
+++ My-React-Portfolio/src/components/Resume.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 function Resume() {
   return (
     <section id="resume">
-      <h2>My Resume</h2>
-      <a href="../assets/images/Resume.png" download>Download Resume</a>
+      <h2>Resume</h2>
+
+      <h3>Proficiencies</h3>
+      <ul>
+        {/* proficiencies below */}
+        <li>Problem Solving</li>
+        <li>Teamwork</li>
+        <li>Leadership</li>
+      </ul>
+
+      <a href="path/to/your/resume.pdf" download>Download Resume</a>
     </section>
   );
 }
